a925fba6bc466a8e95c2cafe81994eaf74d336c7
Arlandis Lawrence <arlandislawrence@gmail.com>
2015-02-15 12:32:22 -0500

    Got min cut specs passing

--- ruby/mincut.rb
+++ ruby/mincut.rb
@@ -5,7 +5,18 @@ class MinCut
   attr_accessor :graph
 
   def initialize(input)
-    @graph = build_graph(input)
+    @graph = Graph.new(input)
+  end
+
+  def min_cut_edge
+    while graph.remaining_vertices.count > 2
+      graph.contract_random_edge
+    end
+    graph.edges.first
+  end
+
+  def edges
+    graph.edges
   end
 
   def min_cut
@@ -13,47 +24,82 @@ class MinCut
   end
 
   def contract(node_one, node_two)
-    graph[node_one].concat(connections_to(node_two))
-    # delete self loops
-    # squash old loops
-    graph.delete(node_two)
-  end
-
-  def edges
-    graph.reduce([]) do |edges, (node, connections)|
-      connections.each do |conn|
-      edges << [node, conn]
-    end
-    edges
-    end
+    graph.contract([node_one, node_two])
   end
 
   def connections_to(node)
     @graph[node]
   end
 
-  private
+  class Graph
 
-  def self_loop?(arr, node_one, node_two)
-    node_one == node_two ||
-    (arr[0] == node_one && arr[1] == node_two) ||
-    (arr[1] == node_two && arr[0] == node_one)
-  end
+    # stored as an adjacency list
+    attr_accessor :edges
 
-  def build_graph(input)
-    input.reduce({}) do |acc, line|
-      acc[parse_node(line)] = parse_connections(line)
-      acc
+    def initialize(input)
+      @edges = build_edges(input)
+    end
+
+    def contract_random_edge
+      edge = edges.sample
+      contract(edge)
+      #new_ref, old_ref = edge
+      #move_ref(new_ref, old_ref)
+      #remove_self_ref_loops(new_ref, old_ref)
+    end
+
+    def contract(edge)
+      new_ref, old_ref = edge
+      @edges = move_ref(new_ref, old_ref)
+      remove_self_ref_loops(new_ref, old_ref)
+      remove_duplicate_edges
+    end
+
+    def [](node)
+      edges.select {|edge| edge.include?(node) }
+    end
+
+    private
+
+    def move_ref(new_ref, old_ref)
+      # [[1, 2], [2, 3]] => [[1, 1], [1, 3]]
+      new_edges = []
+      @edges.each do |edge|
+        if edge[0] == old_ref
+          new_edges << [new_ref, edge[1]].sort
+        elsif edge[1] == old_ref
+          new_edges << [edge[0], new_ref].sort
+        else
+          new_edges << edge
+        end
+      end
+      new_edges
+    end
+
+    def remove_self_ref_loops(node_one, node_two)
+      @edges.each_with_index do |edge, idx|
+        if edge.sort == [node_one, node_two].sort || edge.uniq.count == 1
+          @edges.delete_at(idx)
+        end
+      end
+    end
+
+    def remove_duplicate_edges
+      @edges.uniq! {|item| item.sort}
+    end
+
+    def build_edges(input)
+      input.reduce([]) do |edges, node_and_connections|
+        node = node_and_connections.shift
+        node_and_connections.collect do |conn|
+          edge = [node, conn].sort
+          edges << edge unless edges.include?(edge.sort)
+        end
+        edges
+      end
     end
   end
 
-  def parse_node(line)
-    line.first
-  end
-
-  def parse_connections(line)
-    line[1..-1]
-  end
 end
 
 Util.with_file_arg(__FILE__) do |f|
--- ruby/spec/min_cut_spec.rb
+++ ruby/spec/min_cut_spec.rb
@@ -1,23 +1,11 @@
 require_relative "../mincut"
 
 describe MinCut do
-  describe "#build_graph" do
-    it "parses the first element as a node" do
-      input = [[1, 37, 79, 164, 155, 32,
-                        87, 39, 113, 15, 18, 78,
-                        175, 140, 200, 4, 160, 97,
-                        191, 100, 91, 20, 69, 198, 196]]
+  describe ".new" do
+    it "parses the first element as a node connected to all others" do
+      input = [[1, 37, 79]]
       graph = MinCut.new(input).graph
-      expect(graph.keys).to eq [1]
-    end
-
-    it "parses the remaining nodes as being connected to the first" do
-      input = [[1, 37, 79, 164, 155, 32,
-                        87, 39, 113, 15, 18, 78,
-                        175, 140, 200, 4, 160, 97,
-                        191, 100, 91, 20, 69, 198, 196]]
-      connections = MinCut.new(input).connections_to(1)
-      expect(connections).to eq input.first[1..-1]
+      expect(graph.edges).to eq [[1, 37], [1, 79]]
     end
   end
 
@@ -26,10 +14,7 @@ describe MinCut do
       input = [[1, 2, 3],
                [2, 1, 3],
                [3, 1, 2]]
-      expect(MinCut.new(input).edges).to eq([
-        [1, 2], [1, 3], [2, 1],
-        [2, 3], [3, 1], [3, 2]
-      ])
+      expect(MinCut.new(input).edges).to eq([[1, 2], [1, 3], [2, 3]])
     end
   end
 
@@ -40,15 +25,21 @@ describe MinCut do
                [3, 1, 2]]
       mc = MinCut.new(input)
       mc.contract(1, 2)
-      expect(mc.edges).to eq([[1, 3], [3, 1]])
+      expect(mc.edges).to eq([[1, 3]])
 
+      input = [[1, 2, 3],
+               [2, 1, 3],
+               [3, 1, 2]]
       mc = MinCut.new(input)
       mc.contract(1, 3)
-      expect(mc.edges).to eq([[1, 2], [2, 1]])
+      expect(mc.edges).to eq([[1, 2]])
 
+      input = [[1, 2, 3],
+               [2, 1, 3],
+               [3, 1, 2]]
       mc = MinCut.new(input)
       mc.contract(3, 1)
-      expect(mc.edges).to eq([[3, 2], [2, 3]])
+      expect(mc.edges).to eq([[2, 3]])
     end
   end
 
@@ -64,8 +55,8 @@ describe MinCut do
                [2, 1, 4],
                [3, 1, 4],
                [4, 2, 3]]
-      connections = MinCut.new(input).connections_to(1)
-      expect(connections).to eq input.first[1..-1]
+      edges_with_1 = MinCut.new(input).edges.select {|edge| edge.include?(1)}
+      expect(edges_with_1).to eq([[1, 2], [1, 3]])
     end
   end
 end
